827a617c35f09373b5a70afd5ac13bf15eb6eec5
spaceLover <61938914+kerry-space@users.noreply.github.com>
2022-04-27 21:51:39 +0200

    add initial webiste files

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $("button").addClass("animated bounce");
+    $(".btn").addClass("animated shake");
+
+    $("#target1").addClass("animated fadeOut");
+
+
+
+});
+
